f8790271c93fb95de53df138f33b6908ebcd529a
usmanshan39 <70626124+usmanshan39@users.noreply.github.com>
2022-06-14 18:55:48 +0500

    complete signup component with api call

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { SnackbarService } from '../services/snackbar.service';
+import { UserService } from '../services/user.service';
+import { GlobalConstants } from '../shared/global-constants';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +12,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  signupForm:any = FormGroup;
+  responseMessage:any;
+  buttonDisable:boolean=false;
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder,
+    private router:Router,
+    private userService:UserService,
+    private snackbarService:SnackbarService,
+    private dialogRef:MatDialogRef<SignupComponent>) { }
 
   ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      name:[null, [Validators.required, Validators.pattern(GlobalConstants.nameRegex)]],
+      email:[null, [Validators.required, Validators.pattern(GlobalConstants.emailRegex)]],
+      contactNumber:[null, [Validators.required, Validators.pattern(GlobalConstants.contactNumberRegex)]],
+      password:[null, [Validators.required]],
+    });
+  }
+
+  // signup
+  handleSubmit(){
+    this.buttonDisable = true;
+    var formData = this.signupForm.value;
+    var data={
+      name : formData.name,
+      email: formData.email,
+      contactNumber: formData.contactNumber,
+      password: formData.password
+    }
+    this.userService.signup(data).subscribe((res:any)=>{
+      this.buttonDisable = false;
+      this.dialogRef.close();
+      this.responseMessage = res?.message;
+      this.snackbarService.openSnackbar(this.responseMessage,"");
+      this.router.navigate(['/']);
+    },(error:any)=>{
+      this.buttonDisable=false;
+      if(error.error.message){
+        this.responseMessage = error.error.message;
+      }
+      else{
+        this.responseMessage = GlobalConstants.genericError;
+      }
+      this.snackbarService.openSnackbar(this.responseMessage,GlobalConstants.error);
+    })
   }
 
 }
